9f591d6ad90fefc107985c23a18096baafe13665
Anubhab Swain <62087261+anubhab-code@users.noreply.github.com>
2020-05-15 01:43:16 +0530

    Create Day 3 - Intro to Conditional Statements.py

---
+++ 30 Days of Code/Day 3 - Intro to Conditional Statements.py
@@ -0,0 +1,16 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    N = int(input())
+    if N % 2 == 1:
+        print('Weird')
+    elif N < 5:
+        print('Not Weird')
+    elif N <= 20:
+        print('Weird')
+    else:
+        print('Not Weird')
