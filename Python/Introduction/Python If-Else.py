ce5d933cc6045c1ac31449b6a4c07e20c4384a1e
Anubhab Swain <62087261+anubhab-code@users.noreply.github.com>
2020-05-16 19:28:14 +0530

    Create Python If-Else.py

---
+++ Python/Introduction/Python If-Else.py
@@ -0,0 +1,12 @@
+N = int(input().strip())
+n= N
+w = 'Weird'
+nw = 'Not Weird'
+if n % 2 == 1:
+    print(w)
+elif n % 2 == 0 and (n>=2 and n<5):
+    print(nw)
+elif n % 2 == 0 and (n>=6 and n<=20):
+    print(w)
+elif n % 2 == 0 and (n>20):
+    print(nw) 
