f6110bc5501ea5c1821f5bd95df36062ca6d1995
Anubhab Swain <62087261+anubhab-code@users.noreply.github.com>
2020-06-11 15:28:16 +0530

    Create Day 28 - RegEx, Patterns, and Intro to Databases.py

---
+++ 30 Days of Code/Day 28 - RegEx, Patterns, and Intro to Databases.py
@@ -0,0 +1,27 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    N = int(input())
+
+    pattern = r"@gmail\.com$"
+    regex = re.compile(pattern)
+    firstNames = []
+
+    for N_itr in range(N):
+        firstNameEmailID = input().split()
+
+        firstName = firstNameEmailID[0]
+
+        emailID = firstNameEmailID[1]
+
+        if regex.search(emailID):
+            firstNames.append(firstName)
+
+    firstNames.sort()
+
+    for name in firstNames:
+        print(name)
