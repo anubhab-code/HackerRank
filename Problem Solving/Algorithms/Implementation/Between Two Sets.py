46393e446e606550a68345be9b720ddf079ea677
Anubhab Swain <62087261+anubhab-code@users.noreply.github.com>
2020-06-19 12:18:41 +0530

    Create Between Two Sets.py

---
+++ Problem Solving/Algorithms/Implementation/Between Two Sets.py
@@ -0,0 +1,2 @@
+_, a, b = input(), list(map(int, input().split())), list(map(int, input().split()))
+print(sum([all(i % x == 0 for x in a) and all(x % i == 0 for x in b) for i in range(max(a), min(b)+1)]))
