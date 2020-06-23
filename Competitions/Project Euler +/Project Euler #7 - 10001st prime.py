3a21b74a5456074fdd494477de346697bbaae88a
Anubhab Swain <62087261+anubhab-code@users.noreply.github.com>
2020-06-23 11:05:50 +0530

    Create Project Euler #7 - 10001st prime.py

---
+++ Competitions/Project Euler +/Project Euler #7 - 10001st prime.py
@@ -0,0 +1,24 @@
+import sys
+from math import ceil, log
+
+N_upper_bound = 10 ** 4 + 1  
+limit = ceil(N_upper_bound * (log(N_upper_bound) + log(log(N_upper_bound))))
+
+limit += 1
+numbers = [True] * limit
+primes = []
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    if len(primes) >= N:
+        print(primes[N - 1])
+        continue
+    for number in range(next(reversed(primes), 1) + 1, limit):
+        if numbers[number]:
+            primes.append(number)
+            for multiple in range(number * number, limit, number):
+                numbers[multiple] = False
+            if len(primes) == N:
+                print(number)
+                break
