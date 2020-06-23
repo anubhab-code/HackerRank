fe1a30cdb19e30287250d4c2d99bf06173bef827
Anubhab Swain <62087261+anubhab-code@users.noreply.github.com>
2020-06-23 11:05:45 +0530

    Create Project Euler #5 - Smallest multiple.py

---
+++ Competitions/Project Euler +/Project Euler #5 - Smallest multiple.py
@@ -0,0 +1,15 @@
+import sys
+
+def gcd(a, b):
+    if b:
+        return gcd(b, a % b)
+    else:
+        return a
+
+T = int(input())
+for _ in range(T):
+    product = 1
+    N = int(input())
+    for number in range(2, N + 1):
+        product *= number // gcd(product, number)
+    print(product)
